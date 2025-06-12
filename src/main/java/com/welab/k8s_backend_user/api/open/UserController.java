42f19d949707f859b0dcb36e0910993ed04cb0a3
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-06-12 14:39:08 +0900

    0.0.4

--- src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
@@ -1,7 +1,10 @@
 package com.welab.k8s_backend_user.api.open;
 
 import com.welab.k8s_backend_user.common.dto.ApiResponseDto;
+import com.welab.k8s_backend_user.domain.dto.SiteUserRegisterDto;
 import com.welab.k8s_backend_user.remote.alim.RemoteAlimService;
+import com.welab.k8s_backend_user.serivce.SiteUserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -13,10 +16,17 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final RemoteAlimService remoteAlimService;
+    private final SiteUserService siteUserService;
+
     @GetMapping(value = "/hello")
     public ApiResponseDto<String> hello() {
         String remoteMessage = remoteAlimService.hello().getData();
         String userResponse = "웰컴 투 백엔드 유저. 리모트 알림 메시지= " + remoteMessage;
         return ApiResponseDto.createOk(userResponse);
+    }
+
+    @PostMapping(value = "/register")
+    public ApiResponseDto<String> register(@RequestBody @Valid SiteUserRegisterDto registerDto) {siteUserService.registerUser(registerDto);
+        return ApiResponseDto.defaultOk();
     }
 }
